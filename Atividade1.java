9301a06e65641d827075db2c3f1cbea726595d07
Alan Gabriel Giacomoni Przyvara <88240260+YiipHuhu@users.noreply.github.com>
2024-02-20 22:36:11 -0300

    Create Atividade1.java

---
+++ Atividade1.java
@@ -0,0 +1,23 @@
+// Online Java Compiler
+// Use this editor to write, compile and run your Java code online
+
+class HelloWorld {
+    public static void main(String[] args) {
+        
+        String nome = "Alan";
+        String classe = "Mago";
+        float vida = 10.0f;
+        int poder = 7;
+        char sexo = 'F';
+        boolean Vivo = true;
+        String arma = "Cajado de Cristal";
+        double critico = 1.0004;
+        String boss = "Lagarto";
+        float vidaBoss = 40;
+        boolean vivoBoss = true;
+        
+        System.out.printf("O  %s %s esta vivo e possui atualmente %f de vida, e esta lutando contra o Chefe %s enquanto usa seu %s", classe, nome, vida, boss, arma);
+       
+        
+    }
+}
