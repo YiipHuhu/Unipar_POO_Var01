a51941bde40df4079272c26e20985f884629a70b
Alan Gabriel Giacomoni Przyvara <88240260+YiipHuhu@users.noreply.github.com>
2024-02-24 21:39:11 -0300

    Create Atividade2.java

---
+++ Atividade2.java
@@ -0,0 +1,18 @@
+// Online Java Compiler
+// Use this editor to write, compile and run your Java code online
+
+class HelloWorld {
+    public static void main(String[] args) {
+        String nomeProduto = "Processador Ryzen 5 5600";
+        double precoProduto = 789.90;
+        String marcaDoProduto = "AMD";
+        int quantidadeProduto = 19;
+        String descricaoProduto = "Descrição do produto: O Ryzen 5 5600 conta com 6 núcleos incríveis para quem quer apenas jogar. Os processadores AMD Ryzen série 5000 capacitam a próxima geração de jogos exigentes, proporcionando experiências imersivas únicas e dominando qualquer tarefa multithread como 3D e renderização de vídeo e compilação de software.";
+        
+        String nomeDoProduto123 = "RX 6750 XT";
+        double precodoProduto = 789.90;
+        int numeroDoProduto = 6750;
+        int estoqueDisponivel = 23;
+        int totalDeProdutos = 15;
+    }
+}
